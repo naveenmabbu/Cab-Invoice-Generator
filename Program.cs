d3d2f708b194631bfc257c32030f9c8b689e3e6d
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-16 21:09:10 +0530

    enhanced invoice

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using CabInvoiveGenerator;
+
+Console.WriteLine("Welcome to Cab Invoice Generator");
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+double fare = invoiceGenerator.CalculateFare(2.0, 5);
+Console.WriteLine($"Fare : {fare}");
+Console.ReadLine();// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
